e25ceb9fa6cccb2408d79017c7f8f50a9e6e02b8
Javier Castillo <javiercame98@gmail.com>
2026-01-22 11:13:21 +0100

    feat: creating verifications

--- pizzeria_queries.sql
+++ pizzeria_queries.sql
@@ -8,7 +8,6 @@ FROM cliente c
          JOIN localidad l ON c.id_localidad = l.id_localidad
          JOIN provincia p ON l.id_provincia = p.id_provincia;
 
-
 SELECT
     pe.id_pedido,
     pe.fecha,
@@ -21,3 +20,18 @@ FROM pedido pe
          JOIN tienda t ON pe.id_tienda = t.id_tienda
          LEFT JOIN reparto r ON pe.id_pedido = r.id_pedido
          LEFT JOIN empleado e ON r.id_empleado = e.id_empleado;
+
+SELECT SUM(pp.cantidad) AS total_bebidas_vendidas
+FROM pedido_producto pp
+         JOIN producto p ON pp.id_producto = p.id_producto
+         JOIN pedido pe ON pp.id_pedido = pe.id_pedido
+         JOIN tienda t ON pe.id_tienda = t.id_tienda
+         JOIN localidad l ON t.id_localidad = l.id_localidad
+WHERE p.tipo_producto = 'Bebida'
+  AND l.nombre = 'Barcelona';
+
+
+SELECT e.nombre, e.apellido, COUNT(r.id_pedido) AS total_pedidos_repartidos
+FROM empleado e
+         JOIN reparto r ON e.id_empleado = r.id_empleado
+WHERE e.id_empleado = 1;
